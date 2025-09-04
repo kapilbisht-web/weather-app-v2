84561bc0c8b7ec435ce0072db1d5cf64100b7ad1
Kapil Bisht <kapilbisht006@gmail.com>
2025-09-04 19:26:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,6 +24,19 @@ function App() {
  const fetchWeather = async () => {
   const apiUrl = process.env.REACT_APP_API_URL;
   const apiKey = process.env.REACT_APP_API_KEY;
+   console.log("API URL:", apiUrl);
+console.log("API Key:", apiKey);
+console.log("Test Var:", process.env.REACT_APP_TEST_VAR);
+   console.log("API URL:", apiUrl);
+console.log("API Key:", apiKey);
+console.log("Test Var:", process.env.REACT_APP_TEST_VAR);console.log("API URL:", apiUrl);
+console.log("API Key:", apiKey);
+console.log("Test Var:", process.env.REACT_APP_TEST_VAR);console.log("API URL:", apiUrl);
+console.log("API Key:", apiKey);
+console.log("Test Var:", process.env.REACT_APP_TEST_VAR);console.log("API URL:", apiUrl);
+console.log("API Key:", apiKey);
+console.log("Test Var:", process.env.REACT_APP_TEST_VAR);
+   
 if (!apiUrl || !apiKey) {
   alert("API configuration missing. Check your .env file.");
   return;
